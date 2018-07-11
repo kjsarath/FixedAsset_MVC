23f95e205ec8a3ae72507fde88dd4a8029935a87
SARATH <jayachandran.sarath@yahoo.com>
2018-07-11 11:24:06 +0530

    commit

---
+++ FixedAsset_MVC/DAL/UserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+using FixedAsset_MVC.Models;
+
+namespace FixedAsset_MVC.DAL
+{
+    public class UserService
+    {
+        public IMongoDatabase mDb;
+        public MongoDB.Driver.IMongoCollection<Users> user;
+        public bool serverDown = false;
+        public UserService()
+        {
+            //var mClient=new MongoClient(FixedAsset_MVC.)
+        }
+    }
+}
---
+++ FixedAsset_MVC/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FixedAsset_MVC.Models
+{
+    public class Users
+    {
+        [BsonId]
+        public MongoDB.Bson.ObjectId _id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+        public bool active { get; set; }
+
+    }
+}
