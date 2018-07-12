9ad1ad4216a2f3761dcd49d1760c795adf8db263
SARATH <jayachandran.sarath@yahoo.com>
2018-07-12 18:43:43 +0530

    commit

---
+++ FixedAsset_MVC/ScriptsNg/Controllers/HomeController.js
@@ -0,0 +1,26 @@
+﻿var faApp = angular.module('faApp', ['ngRoute']);
+
+faApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/Login.html',
+            controller: 'LoginController'
+        })
+        .when('/Next', {
+            templateUrl: 'pages/Next.html',
+            controller: 'LoginController'
+        })
+        .when('/Home', {
+            templateUrl: 'pages/Home.html',
+            controller: 'HomeController'
+        })
+});
+
+faApp.controller('HomeController', ['$scope', '$log', '$http', '$location', '$window', function ($scope, $log, $http, $location, $window) {
+    $scope.logInStatus = 'Logout';
+
+    $scope.logOut = function () {
+        //$window.location.href = '/Login.html';
+    };
+
+}])
--- FixedAsset_MVC/ScriptsNg/Controllers/LoginController.js
+++ FixedAsset_MVC/ScriptsNg/Controllers/LoginController.js
@@ -1,45 +1,119 @@
-﻿var faApp = angular.module('faApp', ['ngRoute']);
+﻿
+var faAppAuth=angular.module('Authentication', []);
+var faAppHome = angular.module('Home', []);
+var faAppLogIn = angular.module('faAppLogIn', [
+    'Authentication',
+    'Home',
+    'ngRoute',
+    'ngCookies'
+]);
 
-faApp.config(function ($routeProvider) {
+faAppLogIn.config(['$routeProvider', function ($routeProvider) {
+
     $routeProvider
+        .when('/Login', {
+            controller: 'LoginController',
+            templateUrl: 'pages/login.html',
+            hideMenus: true
+        })
+
         .when('/', {
-            templateUrl: 'pages/Login.html',
-            controller:'LoginController'
+            controller: 'HomeController',
+            templateUrl: 'home.html'
         })
-        .when('/Next', {
-            templateUrl: 'pages/Next.html',
-            controller:'LoginController'
-        })
-});
 
-faApp.controller('LoginController',['$scope','$log','$http', function ($scope, $log, $http) {
-    $scope.userModel = {};
-    $scope.message = '';
-    $scope.allUsers = null;
-    $scope.username = '';
-    $scope.password = '';
+        .otherwise({ redirectTo: '/login' });
+}])
 
-    $scope.getUser = function (userModel) {
-        $http.get('Login/GetUser', { params: { username: $scope.username, password: $scope.password } })
+faAppLogIn.run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in
+            if ($location.path() !== '/Login' && !$rootScope.globals.currentUser) {
+                $location.path('/Login');
+            }
+        });
+    }]);
+
+//faAppLogIn.controller('LoginController', ['$scope', '$log', '$http', '$location', '$window', function ($scope, $log, $http, $location, $window) {
+//    $scope.userModel = {};
+//    $scope.message = '';
+//    $scope.allUsers = null;
+//    $scope.username = '';
+//    $scope.password = '';
+//    $scope.logInStatus = 'Login';
+
+//    $scope.getUser = function (userModel) {
+//        $http.get('Login/GetUser', { params: { username: $scope.username, password: $scope.password } })
+//            .then(function (data, status, header, config) {
+//                if (data == null || data.data=="")
+//                {
+//                    $scope.userModel = {};
+//                    $scope.message = 'Invalid user..!';
+//                    $log.error($scope.message);
+//                }
+//                else
+//                {
+//                    $scope.userModel = data.data;
+//                    $scope.message = 'Valid user :)';
+//                    $log.info($scope.message);
+//                    //$location.path('/Home');
+//                    $window.location.href = '/Home.html';
+//                }
+                
+//            }, function error(data, status, header, config) {
+//                $scope.message = 'Error in fetching data..!';
+//                $log.alert($scope.message);
+//                $log.error(data.statusText);
+//            })
+            
+//    };
+//}]);
+
+faAppLogIn.controller('LoginController',
+    ['$scope', '$rootScope', '$location',function ($scope, $rootScope, $location) {
+
+        $rootScope.globals = {};
+        $cookieStore.remove('globals');
+        $http.defaults.headers.common.Authorization = 'Basic ';
+
+        $scope.login = function () {
+            $http.get('Login/GetUser', { params: { username: $scope.username, password: $scope.password } })
             .then(function (data, status, header, config) {
-                if (data == null || data.data=="")
-                {
+                if (data == null || data.data == "") {
                     $scope.userModel = {};
                     $scope.message = 'Invalid user..!';
                     $log.error($scope.message);
                 }
-                else
-                {
+                else {
                     $scope.userModel = data.data;
                     $scope.message = 'Valid user :)';
                     $log.info($scope.message);
+
+                    $rootScope.globals = {
+                        currentUser: {
+                            username: data.data.username,
+                            password: data.data.password
+                        }
+                    };
+                    $http.defaults.headers.common['Authorization'] = 'Basic ' + data.data.username; // jshint ignore:line
+                    $cookieStore.put('globals', $rootScope.globals);
+
+                    $location.path('/');
+                    //$window.location.href = '/Home.html';
                 }
-                
+
             }, function error(data, status, header, config) {
                 $scope.message = 'Error in fetching data..!';
                 $log.alert($scope.message);
                 $log.error(data.statusText);
             })
-            
-    };
-}]);
+        };
+    }]);
+
